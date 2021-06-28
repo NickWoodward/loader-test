cee2edf3bf15417e4ff4bc2acfd82360cd6a0fd0
Nick Woodward <nick.woodward@hotmail.co.uk>
2021-06-28 18:46:45 +0100

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+import '../assets/icons/webpack.svg';
+
+(() => console.log('webpack test'))();
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SpriteLoaderPlugin = require("svg-sprite-loader/plugin");
+
+
+module.exports = {
+    entry: {
+        index: './src/js/app.js',
+        
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-sprite-loader',
+                        options: {
+                            extract: true,
+                            spriteFilename: 'svg/spritesheet.svg'
+                        },
+                    },
+                    'svgo-loader',
+                ]
+            },
+           
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html',
+            chunks: ['index']
+        }),
+        new SpriteLoaderPlugin(),
+    ]
+}
